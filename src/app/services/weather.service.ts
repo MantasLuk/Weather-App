3161c19a701309b32aa88f424794d9b0c04f856b
Mantas Luk <mantasluk7@gmail.com>
2022-05-16 10:07:52 +0300

    services notes added

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -12,9 +12,12 @@ export class WeatherService {
 
   constructor(private http:HttpClient) {}
 
+  //getting main information about rurrent weather
   public loadCurrentWeather(cityName:string){
     return this.http.get<DetailsCurrent>(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${this.APIkey}&units=metric`)
   }
+
+  //getting information about forecast weather
   public loadForecastWeather(cityName:string){
     return this.http.get<DetailsForecast>(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&cnt=10&appid=${this.APIkey}&units=metric`)
   }
